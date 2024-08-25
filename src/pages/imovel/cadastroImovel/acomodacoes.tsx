f2a555de85039a5cbad4da710f0455a9a51b78ba
Rodrigo Brito <rodrigobrit08t@gmail.com>
2024-08-25 12:33:18 -0300

    criação de rotas das páginas de cadastro de imovel

---
+++ src/pages/imovel/cadastroImovel/acomodacoes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Acomodacoes: React.FC = () => {
+  return (
+    <div>
+      <h1>Acomodações do Imóvel</h1>
+      <p>Informe as acomodações disponíveis no imóvel.</p>
+    </div>
+  );
+};
+
+export default Acomodacoes;
