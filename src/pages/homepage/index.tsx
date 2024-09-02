5837f96a5f52e5a6f18ffb9ef0ba0068703f99dc
lucasbatistaa <lucas.lucas2004.30@gmail.com>
2024-09-02 20:35:22 -0300

    created branch Homepage

---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+import Banner from '../../assets/banner.png'
+
+
+export default function Homepage() {
+    return (
+        <main className="flex min-h-screen px-32 py-[68px]">
+            {/* <Image src={Banner} alt="Banner principal de anúncio" /> */}
+        </main>
+    )
+}
