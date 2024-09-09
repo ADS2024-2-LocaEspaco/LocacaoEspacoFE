50f2b53f38b70d1e6a1e954773849b5a85e27f7a
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-09-09 11:32:29 -0300

    fixed error for appplying global font styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+import { Josefin_Sans, Open_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  variable: "--font-title",
+});
+
+const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-body",
+});
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${josefin.variable} ${openSans.variable} bg-white text-black-100`}>
+      <Component {...pageProps} />;
+    </div>
+  )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,14 @@
-import { Josefin_Sans, Open_Sans } from "next/font/google";
-
-const josefin = Josefin_Sans({
-  subsets: ["latin"],
-  variable: "--font-title",
-});
-
-const openSans = Open_Sans({
-  subsets: ["latin"],
-  variable: "--font-body",
-});
-
 export default function Home() {
   return (
     <main
-      className={`flex flex-col min-h-screen ${josefin.variable} ${openSans.variable}`}
+      className={`flex flex-col min-h-screen`}
     >
       <h1 className="font-title text-5xl font-bold">Teste</h1>
       <div className="font-body">
         <h2 className="font-title">Teste</h2>
-        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur commodi minima sunt quos vero cupiditate omnis distinctio sapiente, dolores nisi dignissimos architecto inventore est deleniti? Officiis eum cupiditate explicabo corporis.</p>
+        <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur commodi minima sunt quos vero cupiditate omnis distinctio sapiente, dolores nisi dignissimos architecto inventore est deleniti? Officiis eum cupiditate explicabo corporis.</p>
 
       </div>
     </main>
-  );
+  )
 }
