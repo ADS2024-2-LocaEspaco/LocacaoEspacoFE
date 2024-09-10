68c1e983b77452051a7c6d885125a51dd465c721
lucasBatistaa <lucas.lucas2004.30@gmail.com>
2024-09-10 11:59:06 -0300

    fix: navigation SliderCarousel

--- src/pages/homepage/components/sliderCarousel.tsx
+++ src/pages/homepage/components/sliderCarousel.tsx
@@ -1,6 +1,7 @@
+import { useRef } from 'react';
 import Image from 'next/image';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperSlide, useSwiper } from 'swiper/react';
 import { Navigation } from "swiper/modules";
 import 'swiper/css';
 import 'swiper/css/navigation'
@@ -11,28 +12,26 @@ import ArrowBackIcon from '../../../../public/icons/arrow_back_icon.svg'
 import ArrowForwardIcon from '../../../../public/icons/arrow_forward_icon.svg'
 
 interface SliderCarouselProps {
-    totalCardsToAppearInScreen: number
+    id: string
     dataSectionMostReserved: CardAdProps[]
 }
 
-export default function SliderCarousel({ totalCardsToAppearInScreen, dataSectionMostReserved }: SliderCarouselProps) {
+export default function SliderCarousel({ id, dataSectionMostReserved }: SliderCarouselProps) {
     return (
         <div className="relative">
             <Swiper
                 modules={[Navigation]}
                 navigation={{
-                    prevEl: '.arrow-back-icon',
-                    nextEl: '.arrow-next-icon'
+                    prevEl: `.arrow-back-icon-${id}`,
+                    nextEl: `.arrow-next-icon-${id}`
                 }}
-                // spaceBetween={44}
-                slidesPerView={totalCardsToAppearInScreen}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+                spaceBetween={32}
+                slidesPerView={'auto'}
             >
 
                 {
                     dataSectionMostReserved.map((ad, index) => (
-                        <SwiperSlide key={index}>
+                        <SwiperSlide key={index} style={{ width: '260px' }}>
                             <CardAd
                                 id={ad.id}
                                 title={ad.title}
@@ -47,11 +46,11 @@ export default function SliderCarousel({ totalCardsToAppearInScreen, dataSection
                 }
             </Swiper>
 
-            <button className="arrow-back-icon disabled:opacity-90 z-10 absolute left-[-16px] top-1/2 transform -translate-y-1/2 bg-white p-3 rounded-full shadow-lg">
+            <button className={`arrow-back-icon-${id} disabled:opacity-70 z-10 absolute left-[-16px] top-1/2 transform -translate-y-1/2 bg-white p-3 rounded-full shadow-lg`}>
                 <Image src={ArrowBackIcon} alt="Icone de voltar" />
             </button>
 
-            <button className="arrow-next-icon disabled:opacity-90 z-10 absolute right-[-16px] top-1/2 transform -translate-y-1/2 bg-white p-3 rounded-full shadow-lg">
+            <button className={`arrow-next-icon-${id} disabled:opacity-70 z-10 absolute right-[-16px] top-1/2 transform -translate-y-1/2 bg-white p-3 rounded-full shadow-lg`}>
                 <Image src={ArrowForwardIcon} alt="Icone de próximo" />
             </button>
         </div>
--- src/pages/homepage/index.tsx
+++ src/pages/homepage/index.tsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 
-import AdImage from '../../assets/card.png'
 import { CardAdProps } from "./components/cardAd";
 import SliderCarousel from "./components/sliderCarousel";
 
+import AdImage from '../../assets/card.png'
+
 export default function Homepage() {
     const [dataSectionMostReserved, setDataSectionMostReserved] = useState<CardAdProps[]>([])
-    const [totalCardsToAppearInScreen, setTotalCardsToAppearInScreen] = useState(0)
+    const [dataSectionChoicesForYou, setDataSectionChoicesForYou] = useState<CardAdProps[]>([])
 
     useEffect(() => {
         setDataSectionMostReserved([
@@ -65,35 +66,41 @@ export default function Homepage() {
                 value: 1000
             },
         ])
-    }, [])
 
-    useEffect(() => {
-        const calculateVisibleCards = () => {
-            const cardWidth = 300
-            const screenWidth = window.innerWidth - 256
-            const cards = Math.floor(screenWidth / cardWidth)
-            setTotalCardsToAppearInScreen(cards)
-        }
-
-        calculateVisibleCards()
-
-        window.addEventListener('resize', calculateVisibleCards)
-
-        return () => window.removeEventListener('resize', calculateVisibleCards)
+        setDataSectionChoicesForYou([
+            {
+                id: 1,
+                title: 'Mansão Oasis',
+                city: 'Varginha',
+                country: 'Brasil',
+                image: AdImage,
+                rating: 5,
+                value: 1000
+            },
+            {
+                id: 1,
+                title: 'Mansão Oasis',
+                city: 'Varginha',
+                country: 'Brasil',
+                image: AdImage,
+                rating: 5,
+                value: 1000
+            },
+        ])
     }, [])
 
     return (
         <main className="min-h-screen space-y-20 px-32 py-[68px] ">
-            <section className="space-y-9">
+            <section className="space-y-9 z-0">
                 <h1 className="font-title text-4xl font-bold text-black-100">Mais reservados</h1>
 
-                <SliderCarousel totalCardsToAppearInScreen={totalCardsToAppearInScreen} dataSectionMostReserved={dataSectionMostReserved}/>
+                <SliderCarousel id={'mostReserved'} dataSectionMostReserved={dataSectionMostReserved}/>
             </section>
 
             <section className="space-y-9">
                 <h1 className="font-title text-4xl font-bold text-black-100">Escolhas para você</h1>
 
-                <SliderCarousel totalCardsToAppearInScreen={totalCardsToAppearInScreen} dataSectionMostReserved={dataSectionMostReserved}/>
+                <SliderCarousel id={'choices'} dataSectionMostReserved={dataSectionChoicesForYou}/>
             </section>
         </main>
     )
