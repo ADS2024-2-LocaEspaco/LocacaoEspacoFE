01e2ab08f210e59156ae8d1754d9d4f184fa589d
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-10-13 20:43:20 -0300

    feat: multiple select and localStorage

--- src/pages/imovel/cadastroImovel/comodidades.tsx
+++ src/pages/imovel/cadastroImovel/comodidades.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { FaWifi, FaTv, FaCar, FaHammer } from 'react-icons/fa';
 import { TbAirConditioning  } from "react-icons/tb";
 import { MdKitchen, MdLocalLaundryService, MdAttachMoney } from "react-icons/md";
@@ -13,28 +14,47 @@ import '@fontsource/josefin-sans';
 
 interface Amenity {
   name: string;
+  value: number;
   icon: React.ReactNode;
 }
 
 const amenities: Amenity[] = [
-  { name: 'Wi-fi', icon: <FaWifi size={32} /> },
-  { name: 'TV', icon: <FaTv size={32} /> },
-  { name: 'Cozinha', icon: <MdKitchen size={32} /> },
-  { name: 'Maquina de Lavar', icon: <MdLocalLaundryService size={32} /> },
-  { name: 'Ar-Condicionado', icon: <TbAirConditioning size={32} /> },
-  { name: 'Estacionamento', icon: <FaCar size={32} /> },
-  { name: 'Estacionamento Pago', icon: <MdAttachMoney size={32} /> },
-  { name: 'Espaço de Trabalho', icon: <FaHammer size={32} /> },
-  { name: 'Ventilador', icon: <PiFanFill size={32} /> },
+  { name: 'Wi-fi', value: 1 , icon: <FaWifi size={32} /> },
+  { name: 'TV', value: 2, icon: <FaTv size={32} /> },
+  { name: 'Cozinha', value: 3, icon: <MdKitchen size={32} /> },
+  { name: 'Maquina de Lavar', value: 4, icon: <MdLocalLaundryService size={32} /> },
+  { name: 'Ar-Condicionado', value: 5, icon: <TbAirConditioning size={32} /> },
+  { name: 'Estacionamento', value: 6, icon: <FaCar size={32} /> },
+  { name: 'Estacionamento Pago', value: 7, icon: <MdAttachMoney size={32} /> },
+  { name: 'Espaço de Trabalho', value: 8, icon: <FaHammer size={32} /> },
+  { name: 'Ventilador', value: 9, icon: <PiFanFill size={32} /> },
 ];
 
 const Comodidade: React.FC = () => {
   const { goToPreviousPage, goToNextPage } = useNavigation();
-  const [selectedItem, setSelectedItem] = React.useState<string | null>(null);
+  const [selectedItems, setSelectedItems] = React.useState<Amenity[]>([]);
 
-  const handleSelect = (item: string) => {
-    setSelectedItem(item);
-  };
+  useEffect(() => {
+    const storedAmenities = localStorage.getItem("@form:comodidade");
+    if (storedAmenities) {
+      const parsedAmenities = JSON.parse(storedAmenities);
+      setSelectedItems(parsedAmenities);
+    }
+  }, []);
+  
+  function handleSelect(amenity: Amenity) {
+    const isSelected = selectedItems.some(item => item.name === amenity.name);
+
+    let updatedSelection;
+    if (isSelected) {
+      updatedSelection = selectedItems.filter(item => item.name !== amenity.name);
+    } else {
+      updatedSelection = [...selectedItems, amenity];
+    }
+
+    setSelectedItems(updatedSelection);
+    localStorage.setItem("@form:comodidade", JSON.stringify(updatedSelection));
+  }
 
   return (
     // Left Side
@@ -50,27 +70,28 @@ const Comodidade: React.FC = () => {
       </div>
 
       {/* Right Side */}
-      <div className="w-1/2 flex flex-col justify-center items-center p-4 bg-white">
-      <h1 className="mb-4 text-[42px] font-semibold leading-[42px] text-center font-josefin text-gray-700">
-        Comodidades
-      </h1>
-        <div className="grid grid-cols-3 gap-10 rounded-md mt-20">
-          {amenities.map((ameneity, index) => (
+        <div className="w-1/2 flex flex-col justify-center items-center p-4 bg-white">
+          <h1 className="mb-4 text-[42px] font-semibold leading-[42px] text-center font-josefin text-gray-700">
+            Comodidades
+          </h1>
+          <div className="grid grid-cols-3 gap-10 rounded-md mt-20">
+            {amenities.map((amenity, index) => (
               <CardSelect
                 key={index}
-                name={ameneity.name}
-                selected={selectedItem === ameneity.name}
-                icon={ameneity.icon}
-                onSelect={() => handleSelect(ameneity.name)}
-            />
-          ))}
-        </div>
-        <div className="flex justify-between items-center w-full mt-4">
-          <IoIosArrowBack className="text-6xl cursor-pointer text-black" onClick={goToPreviousPage}/>
-          <IoIosArrowForward className="text-6xl cursor-pointer text-black" onClick={goToNextPage}/>
+                value={amenity.value}
+                name={amenity.name}
+                selected={selectedItems.some(item => item.name === amenity.name)} // Verificar se está selecionado
+                icon={amenity.icon}
+                onSelect={() => handleSelect(amenity)}
+              />
+            ))}
+          </div>
+          <div className="flex justify-between items-center w-full mt-4">
+            <IoIosArrowBack className="text-6xl cursor-pointer text-black" onClick={goToPreviousPage}/>
+            <IoIosArrowForward className="text-6xl cursor-pointer text-black" onClick={goToNextPage}/>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
--- src/pages/imovel/cadastroImovel/components/CardSelect.tsx
+++ src/pages/imovel/cadastroImovel/components/CardSelect.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-
 interface CardSelectProps {
   name: string;
+  value: string;
   icon?: React.ReactNode;
   selected: boolean;
-  onSelect: () => void;
+  onSelect: (name: string, value: number) => void;
 }
 
-const CardSelect: React.FC<CardSelectProps> = ({ name, icon, selected, onSelect }) => {
+const CardSelect: React.FC<CardSelectProps> = ({ name, value, icon, selected, onSelect }) => {
   return (
     <div
-      onClick={onSelect}
+      onClick={() => onSelect(name, value)} // Passando tanto o name quanto o value
       className={`flex flex-col items-center justify-center border p-4 h-40 w-40 rounded-lg cursor-pointer ${
         selected ? 'border-blue-500 bg-blue-100' : 'border-gray-400 bg-white'
       }`}
