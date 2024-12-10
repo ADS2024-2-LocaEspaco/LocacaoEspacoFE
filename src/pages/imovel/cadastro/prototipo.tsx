252999b479318aed16feb5b4024e6235ac330686
MTavaresBorges <matheustavaresborges2000@gmail.com>
2024-12-10 02:34:20 -0300

    feat: modal in "prototipo"

--- src/pages/imovel/cadastro/prototipo.tsx
+++ src/pages/imovel/cadastro/prototipo.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import Modal from 'react-modal';
 import { CiShare2, CiMap, CiHeart } from "react-icons/ci";
 import { IoBedOutline } from "react-icons/io5";
 import { MdOutlineShower } from "react-icons/md";
@@ -21,7 +22,6 @@ interface Prototipo {
   icon: React.ReactNode;
 }
 
-
 const Prototipo: React.FC = () => {
  
   const [dados, setDados] = useState({
@@ -32,6 +32,9 @@ const Prototipo: React.FC = () => {
     valorReserva: { valor: 0, minDias: 0, maxDias: 0, antecedencia: 0 },
     endereco: [] as any[]
   });
+
+  const [isMainModalOpen, setIsMainModalOpen] = useState(false);
+  const toggleMainModal = () => setIsMainModalOpen(!isMainModalOpen);
 
   const [startDate, setStartDate] = useState(new Date());
  
@@ -56,12 +59,9 @@ const Prototipo: React.FC = () => {
       endereco: Object.keys(endereco).length > 0 ? endereco : []
     });
   }, []);
-  
-
 
   const handleSubmit = async () => {
     try {
-
       const tituloEdescricao = JSON.parse(localStorage.getItem('tituloEdescricao') || '{}');
       const acomodacoes = JSON.parse(localStorage.getItem('acomodacoes') || '{}');
       const comodidades = JSON.parse(localStorage.getItem('comodidades') || '[]');
@@ -175,113 +175,137 @@ const Prototipo: React.FC = () => {
       {/* Right Side */}
       <div className="w-1/2 flex flex-col justify-center items-center p-4 bg-white">
         <h1 className="mb-20 text-[42px] font-semibold leading-[42px] text-center font-josefin text-gray-700">
-          Protótipo
+          Imagens
         </h1>
 
-        <div className="flex flex-col border border-gray-500 p-5 w-full h-auto rounded-2xl font-black text-gray-800 overflow-y-auto"
-          style={{ maxHeight: '700px' }} 
+        <button
+          className="px-12 py-4 text-white bg-blue-500 border rounded-3xl hover:bg-blue-600 focus:outline-none font-josefin"
+          onClick={toggleMainModal}
         >
-          <div className="flex justify-start font-josefin text-2xl">
-              {dados.tituloEdescricao.titulo}
-          </div>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-2 w-full h-auto border border-gray-400 rounded-lg">
-            {/* Primeira linha de imagens */}
-            <img
-              src="https://via.placeholder.com/400x300"
-              alt="Imagem de imóvel"
-              className="w-full h-30 object-cover rounded-lg col-span-1"
-            />
-            <img
-              src="https://via.placeholder.com/400x600"
-              alt="Imagem de imóvel"
-              className="w-full h-full object-cover rounded-lg col-span-1 row-span-2"
-            />
-            <img
-              src="https://via.placeholder.com/400x300"
-              alt="Imagem de imóvel"
-              className="w-full h-30 object-cover rounded-lg col-span-1"
-            />
-            <img
-              src="https://via.placeholder.com/400x300"
-              alt="Imagem de imóvel"
-              className="w-full h-30 object-cover rounded-lg"
-            />
-            <div className="relative w-full h-30 rounded-lg overflow-hidden">
-              <img
-                src="https://via.placeholder.com/400x300"
-                alt="Imagem de imóvel"
-                className="w-full h-full object-cover"
-              />
-            </div>
-          </div>
+          Exibir Protótipo
+        </button>
 
-          <div className="flex w-full mt-4 space-x-4">
-              <div className="flex justify-start gap-2">
-                  {acomodacoesArray.map((item, index) => (
-                  <div key={index} className="border rounded-2xl h-5 w-auto p-4 gap-3 flex items-center space-x-2 font-thin bg-orange-200 font-josefin">
-                      <span className="text-blue-500">
-                          {item.icon}
-                      </span>
-                      {item.amount}
+        <Modal
+          isOpen={isMainModalOpen}
+          onRequestClose={toggleMainModal}
+          contentLabel="Detalhes do Card"
+          className="relative bg-white p-6 rounded-lg shadow-lg max-w-4xl mx-auto mt-20"
+          overlayClassName="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+        >
+
+        <div className="flex flex-col border border-gray-500 p-5 w-full h-auto rounded-2xl font-black text-gray-800 overflow-y-auto"
+                  style={{ maxHeight: '700px' }} 
+                >
+                  <div className="flex justify-start font-josefin text-2xl">
+                      {dados.tituloEdescricao.titulo}
                   </div>
-                  ))}
-              </div>
-              <div className="flex w-full justify-end gap-2">
-                  <button className="flex items-center font-thin gap-2 px-4 py-2 text-white bg-blue-500 border rounded-3xl hover:bg-blue-600 focus:outline-none font-josefin">
-                      <CiMap size={20}/>
-                      Veja o Mapa
-                  </button>
-                  <button className="flex items-center gap-2 px-4 py-2 border rounded-3xl hover:bg-gray-200 focus:outline-none">
-                      <CiShare2 size={20}/>
-                  </button>
-                  <button className="flex items-center gap-2 px-4 py-2 border rounded-3xl hover:bg-gray-200 focus:outline-none">
-                      <CiHeart size={20}/>
-                  </button>
-              </div>
-          </div>
-          <div className="grid grid-cols-12 gap-4">
-            <div className="col-span-7 mt-20">
-              <div className="flex items-center">
-                <img
-                  src="https://via.placeholder.com/400x300"
-                  alt="Imagem de imóvel"
-                  className="w-20 h-20 rounded-full object-cover float-left mr-4"
-                />
-                <h2 className="text-xl font-josefin text-gray-700">
-                  Nome do Proprietário
-                </h2>
-              </div>
-              <div className="p-2">
-                <p className="text-sm font-josefin font-thin text-gray-700">
-                  {dados.tituloEdescricao.descricao}
-                </p>
-              </div>
-            </div>
-            <div className="col-span-5 m-[-8px] mt-20 border border-gray-400 rounded-xl p-6 text-center flex flex-col items-center">
-              <span className="block text-xl font-josefin text-gray-700 text-orange-500">
-                R$ {(dados.valorReserva.valor / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} / diária
-              </span>
+                  <div className="grid grid-cols-2 md:grid-cols-3 gap-4 p-2 w-full h-auto border border-gray-400 rounded-lg">
+                    {/* Primeira linha de imagens */}
+                    <img
+                      src="https://via.placeholder.com/400x300"
+                      alt="Imagem de imóvel"
+                      className="w-full h-30 object-cover rounded-lg col-span-1"
+                    />
+                    <img
+                      src="https://via.placeholder.com/400x600"
+                      alt="Imagem de imóvel"
+                      className="w-full h-full object-cover rounded-lg col-span-1 row-span-2"
+                    />
+                    <img
+                      src="https://via.placeholder.com/400x300"
+                      alt="Imagem de imóvel"
+                      className="w-full h-30 object-cover rounded-lg col-span-1"
+                    />
+                    <img
+                      src="https://via.placeholder.com/400x300"
+                      alt="Imagem de imóvel"
+                      className="w-full h-30 object-cover rounded-lg"
+                    />
+                    <div className="relative w-full h-30 rounded-lg overflow-hidden">
+                      <img
+                        src="https://via.placeholder.com/400x300"
+                        alt="Imagem de imóvel"
+                        className="w-full h-full object-cover"
+                      />
+                    </div>
+                  </div>
 
-              {dados.endereco && (
-                <div className="mt-4 text-gray-700 text-sm font-josefin font-thin">
-                  <p>{dados.endereco.rua}, {dados.endereco.numero}</p>
-                  <p>{dados.endereco.bairro}, {dados.endereco.cidade} - {dados.endereco.uf}</p>
+                  <div className="flex w-full mt-4 space-x-4">
+                      <div className="flex justify-start gap-2">
+                          {acomodacoesArray.map((item, index) => (
+                          <div key={index} className="border rounded-2xl h-5 w-auto p-4 gap-3 flex items-center space-x-2 font-thin bg-orange-200 font-josefin">
+                              <span className="text-blue-500">
+                                  {item.icon}
+                              </span>
+                              {item.amount}
+                          </div>
+                          ))}
+                      </div>
+                      <div className="flex w-full justify-end gap-2">
+                          <button className="flex items-center font-thin gap-2 px-4 py-2 text-white bg-blue-500 border rounded-3xl hover:bg-blue-600 focus:outline-none font-josefin">
+                              <CiMap size={20}/>
+                              Veja o Mapa
+                          </button>
+                          <button className="flex items-center gap-2 px-4 py-2 border rounded-3xl hover:bg-gray-200 focus:outline-none">
+                              <CiShare2 size={20}/>
+                          </button>
+                          <button className="flex items-center gap-2 px-4 py-2 border rounded-3xl hover:bg-gray-200 focus:outline-none">
+                              <CiHeart size={20}/>
+                          </button>
+                      </div>
+                  </div>
+                  <div className="grid grid-cols-12 gap-4">
+                    <div className="col-span-7 mt-20">
+                      <div className="flex items-center">
+                        <img
+                          src="https://via.placeholder.com/400x300"
+                          alt="Imagem de imóvel"
+                          className="w-20 h-20 rounded-full object-cover float-left mr-4"
+                        />
+                        <h2 className="text-xl font-josefin text-gray-700">
+                          Nome do Proprietário
+                        </h2>
+                      </div>
+                      <div className="p-2">
+                        <p className="text-sm font-josefin font-thin text-gray-700">
+                          {dados.tituloEdescricao.descricao}
+                        </p>
+                      </div>
+                    </div>
+                    <div className="col-span-5 m-[-8px] mt-20 border border-gray-400 rounded-xl p-6 text-center flex flex-col items-center">
+                      <span className="block text-xl font-josefin text-gray-700 text-orange-500">
+                        R$ {(dados.valorReserva.valor / 100).toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} / diária
+                      </span>
+
+                      {dados.endereco && (
+                        <div className="mt-4 text-gray-700 text-sm font-josefin font-thin">
+                          <p>{dados.endereco.rua}, {dados.endereco.numero}</p>
+                          <p>{dados.endereco.bairro}, {dados.endereco.cidade} - {dados.endereco.uf}</p>
+                        </div>
+                      )}
+
+                      <div className="mt-6">
+                        <DatePicker
+                          selected={startDate}
+                          onChange={(date: Date) => setStartDate(date)}
+                          inline
+                          className="mt-2 p-2 border border-gray-300 rounded-md text-gray-700 w-full max-w-xs"
+                          calendarClassName="max-h-60 overflow-auto"
+                        />
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              )}
 
-              <div className="mt-6">
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date: Date) => setStartDate(date)}
-                  inline
-                  className="mt-2 p-2 border border-gray-300 rounded-md text-gray-700 w-full max-w-xs"
-                  calendarClassName="max-h-60 overflow-auto"
-                />
-              </div>
-            </div>
-          </div>
+              <button
+                className="absolute top-4 right-4 text-gray-700 hover:text-gray-900"
+                onClick={toggleMainModal}
+              >
+                Fechar
+              </button>
+            </Modal>
 
-        </div>
+        
         <div className="w-full mt-4">
           <label className="inline-flex">
             <input type="checkbox" className="form-checkbox h-5 w-5 text-blue-600" />
